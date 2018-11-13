d05cfcb097178dcd415138e7af46fa6c4f4a3131
chrislo27 <kinchrislo@gmail.com>
2018-11-12 20:10:22 -0800

    Sort out the Gradle stuff

---
+++ gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/RSDE.kt
@@ -0,0 +1,27 @@
+package io.github.chrislo27.rhre3.sfxdb.gui
+
+import io.github.chrislo27.rhre3.sfxdb.gui.util.Version
+import javafx.application.Application
+import javafx.stage.Stage
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+
+class RSDE : Application() {
+
+
+    companion object {
+        val TITLE = "RHRE SFX Database Editor"
+        val VERSION = Version(1, 0, 0, "DEVELOPMENT")
+        val MIN_RHRE_VERSION = Version(3, 16, 0)
+
+        val LOGGER: Logger = LogManager.getContext(RSDE::class.java.classLoader, false).getLogger("RSDE")
+    }
+
+    private lateinit var primaryStage: Stage
+
+    override fun start(primaryStage: Stage) {
+        this.primaryStage = primaryStage
+
+    }
+}
---
+++ main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/TestRSDE.kt
@@ -0,0 +1,11 @@
+package io.github.chrislo27.rhre3.sfxdb
+
+
+object TestRSDE {
+
+    val property = "Test"
+
+    init {
+
+    }
+}
