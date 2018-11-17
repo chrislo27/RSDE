dcd00a09d5040cd80d3028857741d9e359f70c62
chrislo27 <kinchrislo@gmail.com>
2018-11-16 21:22:34 -0800

    Remove immutable class types

--- main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/Game.kt
+++
@@ -1,7 +0,0 @@
-package io.github.chrislo27.rhre3.sfxdb
-
-
-/**
- * A game is a RH minigame.
- */
-data class Game(val id: String, val name: String, val series: Series)
--- main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/datamodel/Datamodel.kt
+++
@@ -1,6 +0,0 @@
-package io.github.chrislo27.rhre3.sfxdb.datamodel
-
-import io.github.chrislo27.rhre3.sfxdb.Game
-
-
-abstract class Datamodel(val game: Game, val id: String, val deprecatedIDs: List<String>, val name: String)
