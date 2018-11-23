d1f9d9c89c74ee7148f051b52eadad351cf4dfd0
chrislo27 <kinchrislo@gmail.com>
2018-11-22 18:50:23 -0800

    Actually set icons map

--- gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/registry/GameRegistry.kt
+++ gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/registry/GameRegistry.kt
@@ -5,6 +5,7 @@ import io.github.chrislo27.rhre3.sfxdb.adt.Datamodel
 import io.github.chrislo27.rhre3.sfxdb.adt.Game
 import io.github.chrislo27.rhre3.sfxdb.gui.RSDE
 import io.github.chrislo27.rhre3.sfxdb.gui.util.JsonHandler
+import javafx.scene.image.Image
 import java.io.File
 
 
@@ -13,12 +14,15 @@ class GameRegistry(val version: Int) {
     @Volatile var isLoaded: Boolean = false
         private set
 
+    val missingIconImage: Image by lazy { Image("image/missing_game_icon.png", true) }
+
     lateinit var gameMap: Map<String, Game>
         private set
     lateinit var datamodelMap: Map<String, Datamodel>
         private set
     lateinit var depsDatamodelMap: Map<String, Datamodel>
         private set
+    lateinit var gameIconMap: Map<Game, Image>
 
     fun loadSFXFolder(progressCallback: (gameObject: kotlin.Result<Game>, loaded: Int, total: Int) -> Unit) {
         isLoaded = false
@@ -29,15 +33,18 @@ class GameRegistry(val version: Int) {
             val folders = rootFolder.listFiles(predicate) /*+ customFolder.listFiles(predicate)*/
             val size = folders.size
             val map = mutableMapOf<String, Game>()
+            val iconMap = mutableMapOf<Game, Image>()
             folders.forEachIndexed { index, folder ->
                 val jsonRoot = JsonHandler.OBJECT_MAPPER.readTree(folder.resolve("data.json"))
                 RSDE.LOGGER.info("Loading game ${folder.name}")
                 val game: Game = Parser.parseGameDefinition(jsonRoot).produceImmutableADT()
                 val gameID = game.id
                 map[gameID] = game
+                iconMap[game] = if (folder.resolve("icon.png").exists()) Image("file:" + folder.resolve("icon.png").path, true) else missingIconImage
                 progressCallback(kotlin.Result.success(game), index + 1, size)
             }
             gameMap = map
+            gameIconMap = iconMap
             val allDatamodels = map.values.flatMap { it.objects }
             val dmMap = mutableMapOf<String, Datamodel>()
             val depsDmMap = mutableMapOf<String, Datamodel>()
