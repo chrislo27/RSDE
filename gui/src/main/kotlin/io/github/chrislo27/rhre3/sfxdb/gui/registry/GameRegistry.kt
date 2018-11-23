35ff242cba806ab062d5db3fa225786af1278c0b
chrislo27 <kinchrislo@gmail.com>
2018-11-22 16:38:55 -0800

    Convert GameRegistry to use immutable ADT

--- gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/registry/GameRegistry.kt
+++ gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/registry/GameRegistry.kt
@@ -1,10 +1,8 @@
 package io.github.chrislo27.rhre3.sfxdb.gui.registry
 
 import io.github.chrislo27.rhre3.sfxdb.Parser
-import io.github.chrislo27.rhre3.sfxdb.validation.DatamodelObject
-import io.github.chrislo27.rhre3.sfxdb.validation.GameObject
-import io.github.chrislo27.rhre3.sfxdb.validation.Result
-import io.github.chrislo27.rhre3.sfxdb.validation.orNull
+import io.github.chrislo27.rhre3.sfxdb.adt.Datamodel
+import io.github.chrislo27.rhre3.sfxdb.adt.Game
 import io.github.chrislo27.rhre3.sfxdb.gui.RSDE
 import io.github.chrislo27.rhre3.sfxdb.gui.util.JsonHandler
 import java.io.File
@@ -15,12 +13,14 @@ class GameRegistry(val version: Int) {
     @Volatile var isLoaded: Boolean = false
         private set
 
-    lateinit var gameMap: Map<String, GameObject>
+    lateinit var gameMap: Map<String, Game>
         private set
-    lateinit var datamodelMap: Map<String, DatamodelObject>
+    lateinit var datamodelMap: Map<String, Datamodel>
+        private set
+    lateinit var depsDatamodelMap: Map<String, Datamodel>
         private set
 
-    fun loadSFXFolder(progressCallback: (gameObject: kotlin.Result<GameObject>, loaded: Int, total: Int) -> Unit) {
+    fun loadSFXFolder(progressCallback: (gameObject: kotlin.Result<Game>, loaded: Int, total: Int) -> Unit) {
         isLoaded = false
         try {
             val rootFolder = RSDE.rhreSfxRoot.resolve("games/")
@@ -28,25 +28,27 @@ class GameRegistry(val version: Int) {
             val predicate = { file: File -> file.isDirectory && file.resolve("data.json").exists() }
             val folders = rootFolder.listFiles(predicate) /*+ customFolder.listFiles(predicate)*/
             val size = folders.size
-            val map = mutableMapOf<String, GameObject>()
+            val map = mutableMapOf<String, Game>()
             folders.forEachIndexed { index, folder ->
                 val jsonRoot = JsonHandler.OBJECT_MAPPER.readTree(folder.resolve("data.json"))
-                val gameObject: GameObject = Parser.parseGameDefinition(jsonRoot)
-                val gameID = (gameObject.id as? Result.Success)?.value
-                if (gameID != null) {
-                    map[gameID] = gameObject
-                }
-                progressCallback(kotlin.Result.success(gameObject), index + 1, size)
+                RSDE.LOGGER.info("Loading game ${folder.name}")
+                val game: Game = Parser.parseGameDefinition(jsonRoot).produceImmutableADT()
+                val gameID = game.id
+                map[gameID] = game
+                progressCallback(kotlin.Result.success(game), index + 1, size)
             }
             gameMap = map
-            val allDatamodels = map.values.asSequence().mapNotNull { it.objects.orNull() }.flatMap { it.asSequence() }.mapNotNull { it.orNull() }.filter { it.id is Result.Success }
-            val dmMap = mutableMapOf<String, DatamodelObject>()
+            val allDatamodels = map.values.flatMap { it.objects }
+            val dmMap = mutableMapOf<String, Datamodel>()
+            val depsDmMap = mutableMapOf<String, Datamodel>()
             allDatamodels.forEach { datamodel ->
-                dmMap[(datamodel.id as Result.Success).value] = datamodel
-                val deprecatedIDs = datamodel.deprecatedIDs.orNull()
-                deprecatedIDs?.forEach { dmMap[it] = datamodel }
+                dmMap[datamodel.id] = datamodel
+                depsDmMap[datamodel.id] = datamodel
+                val deprecatedIDs = datamodel.deprecatedIDs
+                deprecatedIDs.forEach { depsDmMap[it] = datamodel }
             }
             datamodelMap = dmMap
+            depsDatamodelMap = depsDmMap
 
             isLoaded = true
         } catch (e: Exception) {
