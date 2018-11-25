8cf703747499528cb00e714b039f1dbaa4862fb6
chrislo27 <kinchrislo@gmail.com>
2018-11-24 17:48:21 -0800

    Remove no-arg constructors

--- main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/adt/ADT.kt
+++ main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/adt/ADT.kt
@@ -17,9 +17,7 @@ class Game(
         @JsonInclude(JsonInclude.Include.NON_DEFAULT) val noDisplay: Boolean = false,
 
         val objects: List<Datamodel>
-) {
-    constructor() : this("", "", Series.OTHER, objects = listOf())
-}
+)
 
 class CuePointer(
         val id: String,
@@ -29,9 +27,7 @@ class CuePointer(
         @JsonInclude(JsonInclude.Include.CUSTOM, valueFilter = VolumeFilter::class) val volume: Int = 100,
         @JsonInclude(JsonInclude.Include.NON_DEFAULT) val track: Int = 0,
         @JsonInclude(JsonInclude.Include.NON_EMPTY) val metadata: Map<String, Any?>? = null
-) {
-    constructor() : this("")
-}
+)
 
 @JsonPropertyOrder("type", "id", "name", "deprecatedIDs")
 abstract class Datamodel(@JsonInclude(JsonInclude.Include.ALWAYS) val type: String,
@@ -50,56 +46,41 @@ class Cue(
     @JsonInclude(JsonInclude.Include.NON_EMPTY) val responseIDs: List<String>? = null,
     @JsonInclude(JsonInclude.Include.NON_DEFAULT) val baseBpm: Float = 0f,
     @JsonInclude(JsonInclude.Include.NON_DEFAULT) val loops: Boolean = false
-) : Datamodel("cue", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf(), 0f)
-}
+) : Datamodel("cue", id, name, deprecatedIDs)
 class Pattern(
         id: String, name: String, deprecatedIDs: List<String>,
         val cues: List<CuePointer>,
         @JsonInclude(JsonInclude.Include.NON_DEFAULT) val stretchable: Boolean = false
-) : Datamodel("pattern", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf(), listOf())
-}
+) : Datamodel("pattern", id, name, deprecatedIDs)
 class Equidistant(
         id: String, name: String, deprecatedIDs: List<String>,
         val cues: List<CuePointer>,
         val distance: Float = 0f,
         val stretchable: Boolean = false
-) : Datamodel("equidistant", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf(), listOf())
-}
+) : Datamodel("equidistant", id, name, deprecatedIDs)
 
 class KeepTheBeat(
         id: String, name: String, deprecatedIDs: List<String>,
         val cues: List<CuePointer>,
         val defaultDuration: Float = 0f
-) : Datamodel("keepTheBeat", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf(), listOf())
-}
+) : Datamodel("keepTheBeat", id, name, deprecatedIDs)
 
 class RandomCue(
         id: String, name: String, deprecatedIDs: List<String>,
         val cues: List<CuePointer>,
         @JsonInclude(JsonInclude.Include.NON_EMPTY) val responseIDs: List<String>? = null
-) : Datamodel("randomCue", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf(), listOf())
-}
+) : Datamodel("randomCue", id, name, deprecatedIDs)
 
 class SubtitleEntity(
         id: String, name: String, deprecatedIDs: List<String>,
         @JsonInclude(JsonInclude.Include.NON_EMPTY) val subtitleType: String? = null
-) : Datamodel("subtitle", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf())
-}
+) : Datamodel("subtitle", id, name, deprecatedIDs)
 
-class EndRemixEntity(id: String, name: String, deprecatedIDs: List<String>) : Datamodel("endEntity", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf())
-}
+class EndRemixEntity(id: String, name: String, deprecatedIDs: List<String>)
+    : Datamodel("endEntity", id, name, deprecatedIDs)
 
-class ShakeEntity(id: String, name: String, deprecatedIDs: List<String>) : Datamodel("shakeEntity", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf())
-}
+class ShakeEntity(id: String, name: String, deprecatedIDs: List<String>)
+    : Datamodel("shakeEntity", id, name, deprecatedIDs)
 
-class TextureEntity(id: String, name: String, deprecatedIDs: List<String>) : Datamodel("textureEntity", id, name, deprecatedIDs) {
-    constructor() : this("", "", listOf())
-}
+class TextureEntity(id: String, name: String, deprecatedIDs: List<String>)
+    : Datamodel("textureEntity", id, name, deprecatedIDs)
