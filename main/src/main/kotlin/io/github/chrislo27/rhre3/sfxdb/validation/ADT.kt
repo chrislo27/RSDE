bfea6c40ca68003cb6178067e0d90995bc1653cb
chrislo27 <kinchrislo@gmail.com>
2018-11-27 21:30:25 -0800

    Verification ADT is mutable

--- main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/validation/ADT.kt
+++ main/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/validation/ADT.kt
@@ -44,10 +44,10 @@ class CuePointerObject : Struct {
 
     var beat: Result<Float> by Property(Transformers.floatTransformer)
     var duration: Result<Float> by Property(Transformers.positiveFloatTransformer("Duration must be positive", true), 0f)
-    val track: Result<Int> by Property(Transformers.intTransformer, 0)
-    val semitone: Result<Int> by Property(Transformers.intTransformer, 0)
-    val volume: Result<Int> by Property(Transformers.volumeTransformer, 100)
-    val metadata: Result<Map<String, Any?>?> by Property(Transformers.cuePointerMetadataTransformer, null as Map<String, Any?>?)
+    var track: Result<Int> by Property(Transformers.intTransformer, 0)
+    var semitone: Result<Int> by Property(Transformers.intTransformer, 0)
+    var volume: Result<Int> by Property(Transformers.volumeTransformer, 100)
+    var metadata: Result<Map<String, Any?>?> by Property(Transformers.cuePointerMetadataTransformer, null as Map<String, Any?>?)
 
     override fun produceImmutableADT(): CuePointer {
         return CuePointer(
@@ -58,17 +58,17 @@ class CuePointerObject : Struct {
 }
 
 class CueObject : DatamodelObject() {
-    val duration: Result<Float> by Property(Transformers.floatTransformer)
+    var duration: Result<Float> by Property(Transformers.floatTransformer)
 
     // Optional after this line
-    val stretchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
-    val repitchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
-    val fileExtension: Result<String> by Property(Transformers.soundFileExtensionTransformer, "ogg")
-    val loops: Result<Boolean> by Property(Transformers.booleanTransformer, false)
-    val baseBpm: Result<Float> by Property(Transformers.positiveFloatTransformer("Base BPM must be positive"), 0f)
-    val introSound: Result<String> by Property(Transformers.idTransformer, "")
-    val endingSound: Result<String> by Property(Transformers.idTransformer, "")
-    val responseIDs: Result<List<String>> by Property(Transformers.responseIDsTransformer, listOf())
+    var stretchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
+    var repitchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
+    var fileExtension: Result<String> by Property(Transformers.soundFileExtensionTransformer, "ogg")
+    var loops: Result<Boolean> by Property(Transformers.booleanTransformer, false)
+    var baseBpm: Result<Float> by Property(Transformers.positiveFloatTransformer("Base BPM must be positive"), 0f)
+    var introSound: Result<String> by Property(Transformers.idTransformer, "")
+    var endingSound: Result<String> by Property(Transformers.idTransformer, "")
+    var responseIDs: Result<List<String>> by Property(Transformers.responseIDsTransformer, listOf())
 
     override fun produceImmutableADT(): Cue {
         return Cue(
@@ -80,10 +80,10 @@ class CueObject : DatamodelObject() {
 }
 
 class PatternObject : DatamodelObject() {
-    val cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer()))
+    var cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer()))
 
     // Optional after this line
-    val stretchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
+    var stretchable: Result<Boolean> by Property(Transformers.booleanTransformer, false)
 
     override fun produceImmutableADT(): Pattern {
         return Pattern(
@@ -95,9 +95,9 @@ class PatternObject : DatamodelObject() {
 }
 
 class EquidistantObject : DatamodelObject() {
-    val cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer(beatNotUsed = true, durationNotUsed = true)))
-    val distance: Result<Float> by Property(Transformers.positiveFloatTransformer("Distance must be positive, non-zero", false))
-    val stretchable: Result<Boolean> by Property(Transformers.booleanTransformer)
+    var cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer(beatNotUsed = true, durationNotUsed = true)))
+    var distance: Result<Float> by Property(Transformers.positiveFloatTransformer("Distance must be positive, non-zero", false))
+    var stretchable: Result<Boolean> by Property(Transformers.booleanTransformer)
 
     override fun produceImmutableADT(): Equidistant {
         return Equidistant(
@@ -108,8 +108,8 @@ class EquidistantObject : DatamodelObject() {
 }
 
 class KeepTheBeatObject : DatamodelObject() {
-    val cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer()))
-    val defaultDuration: Result<Float> by Property(Transformers.positiveFloatTransformer("Default duration must be positive, non-zero", false))
+    var cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer()))
+    var defaultDuration: Result<Float> by Property(Transformers.positiveFloatTransformer("Default duration must be positive, non-zero", false))
 
     override fun produceImmutableADT(): KeepTheBeat {
         return KeepTheBeat(
@@ -120,10 +120,10 @@ class KeepTheBeatObject : DatamodelObject() {
 }
 
 class RandomCueObject : DatamodelObject() {
-    val cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer(beatNotUsed = true)))
+    var cues: Result<List<Result<CuePointerObject>>> by Property(Transformers.transformerToList(Transformers.cuePointerTransformer(beatNotUsed = true)))
 
     // Optional after this line
-    val responseIDs: Result<List<String>> by Property(Transformers.responseIDsTransformer, listOf())
+    var responseIDs: Result<List<String>> by Property(Transformers.responseIDsTransformer, listOf())
 
     override fun produceImmutableADT(): RandomCue {
         return RandomCue(
