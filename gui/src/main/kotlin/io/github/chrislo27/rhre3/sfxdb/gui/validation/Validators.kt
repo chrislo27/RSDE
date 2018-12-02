0b6f14dfe014e90b609e80eca2737e885e8b24a5
chrislo27 <kinchrislo@gmail.com>
2018-12-02 00:05:53 -0800

    Fix possible NPE

--- gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/validation/Validators.kt
+++ gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/validation/Validators.kt
@@ -65,11 +65,11 @@ object Validators {
 
     // CuePointer
     val EXTERNAL_CUE_POINTER: Validator<String> = Validator { t, u ->
-        fromWarningIf(t, UiLocalization["validation.cuePointerExtDependency"], !u.startsWith("*") && u.isNotEmpty())
+        fromWarningIf(t, UiLocalization["validation.cuePointerExtDependency"], u != null && !u.startsWith("*") && u.isNotEmpty())
     }
 
     fun cuePointerPointsNowhere(gameObj: Game): Validator<String> = Validator { t, u ->
-        fromErrorIf(t, UiLocalization["validation.invalidCuePointer"], u.startsWith("*") && u.isNotEmpty() && u !in gameObj.objects.map { it.id })
+        fromErrorIf(t, UiLocalization["validation.invalidCuePointer"], u != null && u.startsWith("*") && u.isNotEmpty() && u !in gameObj.objects.map { it.id })
     }
 
     // Response IDs
