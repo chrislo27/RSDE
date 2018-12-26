b50e2b1e7a6b59646d0e0fdc91fec9b1ee87c135
chrislo27 <kinchrislo@gmail.com>
2018-12-26 13:34:03 -0800

    Bump version

--- gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/RSDE.kt
+++ gui/src/main/kotlin/io/github/chrislo27/rhre3/sfxdb/gui/RSDE.kt
@@ -32,7 +32,7 @@ class RSDE : Application() {
 
     companion object {
         const val TITLE = "RHRE SFX Database Editor"
-        val VERSION = Version(1, 0, 5, "DEVELOPMENT")
+        val VERSION = Version(1, 0, 5, "")
         val MIN_RHRE_VERSION = Version(3, 16, 0)
         val rootFolder: File = File(System.getProperty("user.home")).resolve(".rsde/").apply { mkdirs() }
         val rhreRoot: File = File(System.getProperty("user.home")).resolve(".rhre3/").apply {
